87edb1082a480b96bcc3b534e89bc26a8c2113c4
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-07-14 21:44:22 -0500

    chnaged the animate on scroll duration: 600 from 1000

--- assets/js/main.js
+++ assets/js/main.js
@@ -222,7 +222,7 @@
    */
   window.addEventListener('load', () => {
     AOS.init({
-      duration: 1000,
+      duration: 600,
       easing: 'ease-in-out',
       once: true,
       mirror: false
